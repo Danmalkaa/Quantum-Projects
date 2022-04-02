45b2750b707fc72cd5c0118998268725e362d7de
danmalkaa <danmalka@mail.tau.co.il>
2022-04-02 12:58:51 +0300

    q1

--- HW1.py
+++ HW1.py
@@ -1,38 +1,25 @@
 from qiskit import (execute ,Aer)
 
 # Use Aer ’s qasm_simulator
-simulator = Aer . get_backend (’ qasm_simulator ’)
+simulator = Aer . get_backend ('qasm_simulator')
 from qiskit import QuantumCircuit
-circuit = QuantumCircuit ( qbits , cbits )
-circuit .h (3)
-# Hadamard gate
-circuit .h( qubit_number )
 
-# X gate
-circuit .x( qubit_number )
+# Q1
+# Hadmard and Measure
+circuit = QuantumCircuit(1, 1)
+circuit.h(0)
+circuit.measure(0,0)
 
-# Z gate
-circuit .z( qubit_number )
-
-# CNOT gate with control qubit control_qubit and target qubit target_qubit
-circuit . cx ( control_qubit , target_qubit )
-
-# Measure the qubits in qibits_subset and store the result in the classical bits cbits_subset .
-# Note that we do not have to measure all of the qubits , but only the subset we choose .
-circuit . measure ( qubits_subset , cbits_subset )
-
-# Draw the circuit
-circuit . draw ( output =’mpl ’, filename =’my_circuit . png ’)
-
-# Execute the circuit on the IBM simulator
-# The simulator will run 1000 simulations of the circuit and will collect the
-measurement results .
-job = execute ( circuit , simulator , shots =1000)
+# Execute the circuit on the qasm simulator
+job = execute(circuit, simulator, shots=1000)
 
 # Grab results from the job
-result = job . result ()
+result = job.result()
 
-# Returns counts of the number of occurences of each eigenstate of the
-computational basis
-counts = result . get_counts ( circuit )
-print ( counts )
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 0 and 1 are:",counts)
+
+# Draw the circuit
+circuit.draw()
+print()
